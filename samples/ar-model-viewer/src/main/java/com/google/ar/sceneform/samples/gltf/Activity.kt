878a3e9535b92824bc78cfc0a1ff183ce44c13da
Wyatt <daviswy@mail.gvsu.edu>
2021-12-10 14:49:36 -0500

    Everything

---
+++ samples/ar-model-viewer/src/main/java/com/google/ar/sceneform/samples/gltf/Activity.kt
@@ -0,0 +1,17 @@
+package com.google.ar.sceneform.samples.gltf
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.commit
+
+class Activity : AppCompatActivity(R.layout.activity) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        supportFragmentManager.commit {
+            add(R.id.containerFragment, MainFragment::class.java, Bundle())
+        }
+    }
+}
